ef8686c8a2b4da79e672edbe62f5b02ce257e32f
Ryan D <git@ryan0x44.com>
2016-03-06 16:41:36 +1100

    Created separate glide config for each app

--- cli/main.go
+++ cli/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/ryan0x44/harveycms/core"
 )
 
 func main() {
 	fmt.Println("cli app")
+	fmt.Println("initialising database")
+	dbInit()
+}
+
+func dbInit() {
+	db := core.DbConnect("dev:password@/harveycms?charset=utf8&parseTime=True&loc=Local")
+	db.CreateTable(&core.Route{})
 }
